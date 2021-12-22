640f163211e58f54a6b8a6df3c196b56ef987e12
BPluijm <75165735+BPluijm@users.noreply.github.com>
2021-12-22 19:36:04 +0100

    nested statements and array

---
+++ src/com/company/Array.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Array {
+
+    public static void main(String[] args) {
+        String[] newArr = new String[5];
+        newArr[0] = "hello";
+        newArr[1] = "hi";
+        newArr[2] = "tim";
+        newArr[3] = "bill";
+        newArr[4] = "joe";
+
+        //        String x = newArr[4];
+
+        int[] nums = {2,3,54,6,6};
+
+
+        double[] nums2 = {2.0, 3.0};
+
+
+        int x = nums[4];
+        double z = nums2[1];
+
+        System.out.println(x);
+        System.out.println(z);
+    }
+}
--- src/com/company/ConditionsBoolean.java
+++ src/com/company/ConditionsBoolean.java
@@ -4,5 +4,15 @@ public class ConditionsBoolean {
 
     public static void main(String[] args) {
 
+        int x = 6;
+        int y = 23;
+        int z = 10;
+
+        // > < == >= <= !=
+
+        boolean compare = !(x < y && z == x);
+
+        System.out.println(compare);
+
     }
 }
